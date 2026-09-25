c1cbab1a12b3093c3fb358e9e2a92ab5ee4aeb30
Tiago David Furtado <tiagofur@MacBook-Pro-de-Tiago.local>
2026-09-25 09:38:30 -0600

    chore(sketchup): bump extension version to 0.1.8 for owner host testing

--- apps/sketchup-extension/src/granete_for_sketchup/identity.rb
+++ apps/sketchup-extension/src/granete_for_sketchup/identity.rb
@@ -4,6 +4,6 @@ module Granete
   module SketchUpExtension
     EXTENSION_ID = 'granete_for_sketchup' unless const_defined?(:EXTENSION_ID, false)
     EXTENSION_NAME = 'Granete for SketchUp' unless const_defined?(:EXTENSION_NAME, false)
-    EXTENSION_VERSION = '0.1.6' unless const_defined?(:EXTENSION_VERSION, false)
+    EXTENSION_VERSION = '0.1.8' unless const_defined?(:EXTENSION_VERSION, false)
   end
 end
